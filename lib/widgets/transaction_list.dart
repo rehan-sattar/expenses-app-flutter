fb4f5875dd73b0fcf54413f19b627742b9b497ee
rehan-sattar <rehansattar117@gmail.com>
2020-02-16 15:37:58 +0500

    feat(style) - add more height to list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 500,
       child: transactions.isEmpty
           ? Column(
               children: <Widget>[
